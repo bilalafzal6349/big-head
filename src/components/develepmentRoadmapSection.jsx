9f2ca363597e2ebd7eaa6512e9d439500c346f4b
M Bilal Afzal <bilalkhanghouri37@gmail.com>
2025-04-12 23:19:48 +0500

    feat:done with some styling

--- src/components/develepmentRoadmapSection.jsx
+++ src/components/develepmentRoadmapSection.jsx
@@ -4,10 +4,10 @@ export const DevelopmentRoadMapSection = () => {
   return (
     <div className="text-center  bg-black text-white">
       <Container>
-        <h2 className="text-center text-5xl font-signatra p-4 py-24 tracking-tighter">
+        <h2 className="text-center text-5xl font-signatra p-4 py-8 md:py-16 lg:py-24 tracking-tighter">
           Road Map
         </h2>
-        <div className="px-8 pb-32">
+        <div className="px-8 md:pb-12 lg:pb-16">
           <div className="custom-shadow order-white border-[3px] border-solid rounded-[50%] max-w-[350px] mx-auto h-[190px] grid justify-center items-center text- bg-transparent font-normal tracking-tighter sm:max-w-[450px]         ">
             <h1 className="text-center text-5xl font-signatra">
               Development Road Map
@@ -15,7 +15,7 @@ export const DevelopmentRoadMapSection = () => {
           </div>
           <div className="   lg:grid lg:grid-cols-2 py-24">
             <div className=" pb-16 ">
-              <p className="text-sm border-[2px] border-solid border-[#FBFAF5] p-16 text-center mx-auto rounded-xl font-Montserrat font-semibold ">
+              <p className="text-sm border-[2px] border-solid border-[#FBFAF5] p-8 md:p-12 lg:p-16 text-center mx-auto rounded-xl font-Montserrat font-semibold ">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Vestibulum tincidunt condimentum dolor, quis sagittis enim
                 facilisis a.
@@ -68,7 +68,7 @@ export const DevelopmentRoadMapSection = () => {
               </div>
             </div>
             <div className=" ">
-              <p className="text-sm border-[2px] border-solid border-[#FBFAF5] p-16 text-center mx-auto rounded-xl font-Montserrat font-semibold">
+              <p className="text-sm border-[2px] border-solid border-[#FBFAF5] p-8 md:p-12 lg:p-16 text-center mx-auto rounded-xl font-Montserrat font-semibold">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Vestibulum tincidunt condimentum dolor, quis sagittis enim
                 facilisis a.
@@ -78,7 +78,7 @@ export const DevelopmentRoadMapSection = () => {
               </h2>
             </div>
             <div className="">
-              <p className="text-sm border-[2px] border-solid border-[#FBFAF5] p-16 text-center mx-auto rounded-xl  font-Montserrat font-semibold">
+              <p className="text-sm border-[2px] border-solid border-[#FBFAF5] p-8 md:p-12 lg:p-16 text-center mx-auto rounded-xl  font-Montserrat font-semibold">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Vestibulum tincidunt condimentum dolor, quis sagittis enim
                 facilisis a.
@@ -110,7 +110,7 @@ export const DevelopmentRoadMapSection = () => {
             </div>
             <div className="w-full    text-white"></div>
             <div className="">
-              <p className="border-[2px] border-solid border-[#FBFAF5] p-16 text-center mx-auto rounded-xl text-sm font-Montserrat font-semibold">
+              <p className="border-[2px] border-solid border-[#FBFAF5] p-8 md:p-12 lg:p-16 text-center mx-auto rounded-xl text-sm font-Montserrat font-semibold">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Vestibulum tincidunt condimentum dolor, quis sagittis enim
                 facilisis a.
--- src/components/launchroadmap.jsx
+++ src/components/launchroadmap.jsx
@@ -3,18 +3,18 @@ export const LaunchRoadMap = () => {
   return (
     <div className="text-center bg-black  text-white pt-24" id="road-map">
       <Container>
-        <h2 className="text-center text-5xl font-signatra p-4 py-24 tracking-tighter">
+        <h2 className="text-center text-5xl font-signatra p-4 py-8 md:py-16 lg:py-24 tracking-tighter">
           Road Map
         </h2>
-        <div className="px-8 pb-32">
+        <div className="px-8 pb-8 md:pb-12 lg:pb-16">
           <div className="custom-shadow border-white border-[3px] border-solid rounded-[50%] max-w-[350px] mx-auto h-[190px] grid justify-center items-center text- bg-transparent font-normal tracking-tighter sm:max-w-[450px]  ">
             <h1 className="text-center text-5xl font-signatra">
               Launch Road Map
             </h1>
           </div>
-          <div className="   lg:grid lg:grid-cols-2 py-24  ">
+          <div className="   lg:grid lg:grid-cols-2 pt-24  ">
             <div className=" pb-16 ">
-              <p className="text-sm border-[2px] border-solid border-[#FBFAF5] p-16 text-center mx-auto rounded-xl font-Montserrat font-semibold ">
+              <p className="text-sm border-[2px] border-solid border-[#FBFAF5] p-8 md:p-12 lg:p-16 text-center mx-auto rounded-xl font-Montserrat font-semibold ">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Vestibulum tincidunt condimentum dolor, quis sagittis enim
                 facilisis a.
@@ -67,7 +67,7 @@ export const LaunchRoadMap = () => {
               </div>
             </div>
             <div className=" ">
-              <p className="text-sm border-[2px] border-solid border-[#FBFAF5] p-16 text-center mx-auto rounded-xl font-Montserrat font-semibold">
+              <p className="text-sm border-[2px] border-solid border-[#FBFAF5] p-8 md:p-12 lg:p-16 text-center mx-auto rounded-xl font-Montserrat font-semibold">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Vestibulum tincidunt condimentum dolor, quis sagittis enim
                 facilisis a.
@@ -75,7 +75,7 @@ export const LaunchRoadMap = () => {
               <h2 className="pt-8 pb-24 text-2xl font-bold">Event Access</h2>
             </div>
             <div className="">
-              <p className="text-sm border-[2px] border-solid border-[#FBFAF5] p-16 text-center mx-auto rounded-xl  font-Montserrat font-semibold">
+              <p className="text-sm border-[2px] border-solid border-[#FBFAF5] p-8 md:p-12 lg:p-16 text-center mx-auto rounded-xl  font-Montserrat font-semibold">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Vestibulum tincidunt condimentum dolor, quis sagittis enim
                 facilisis a.
@@ -106,7 +106,7 @@ export const LaunchRoadMap = () => {
             </div>
             <div className="w-full     text-white"></div>
             <div className="">
-              <p className="border-[2px] border-solid border-[#FBFAF5] p-16 text-center mx-auto rounded-xl text-sm font-Montserrat font-semibold">
+              <p className="border-[2px] border-solid border-[#FBFAF5] p-8 md:p-12 lg:p-16 text-center mx-auto rounded-xl text-sm font-Montserrat font-semibold">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Vestibulum tincidunt condimentum dolor, quis sagittis enim
                 facilisis a.
